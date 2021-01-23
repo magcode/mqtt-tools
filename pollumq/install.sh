6fdd3b631872cbd968c626da0517d95add6b7c94
magcode <marko.web1@outlook.com>
2021-01-23 21:29:47 +0100

    Update install.sh

--- pollumq/install.sh
+++ pollumq/install.sh
@@ -1,5 +1,27 @@
 gcc pollucom.c -o pollucom
 cd mqtt
 npm install
-sudo node install.js
+
+home=`pwd`
+user=`whoami`
+
+__service="
+[Unit]
+Description=PolluComE MQTT
+After=network.target
+[Service]
+ExecStart=node $home/mqtt-tools/pollumq/mqtt/pollumq.js
+WorkingDirectory=$home/mqtt-tools/pollumq/mqtt
+StandardOutput=inherit
+StandardError=inherit
+Restart=always
+User=$user
+[Install]
+WantedBy=multi-user.target
+"
+
+echo "$__service" | sudo tee /etc/systemd/system/pollumq.service
+sudo systemctl daemon-reload
+sudo systemctl enable pollumq.service
+
 echo "Done. Start with 'sudo service pollumq start'."
