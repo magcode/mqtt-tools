bfab2b0ae03b66c6b002a0e4eed5a6dc3335419d
magcode <mdweb+github@posteo.de>
2023-04-30 11:56:32 +0200

    allow syslog

--- switchbotmq/Logger.js
+++ switchbotmq/Logger.js
@@ -1,12 +1,32 @@
 const { createLogger, transports, format } = require("winston");
 const { combine, timestamp, splat, colorize, printf } = format;
 const LokiTransport = require("winston-loki");
+require('winston-syslog').Syslog;
 require('winston-daily-rotate-file');
 
 class LoggerFactory {
     constructor(config, appname) {
         this.logger = createLogger();
+        if (config.syslog.enabled) {
+            const slOptions = {
+                "host": config.syslog.host,
+                "port": config.syslog.port,
+                "type": config.syslog.type,
+                "app_name": appname,
+                "eol": " ",
+                "protocol": config.syslog.protocol,
+                "format": format.combine(
+                    format.splat(),
+                    format.simple()
+                )
+            };
+            this.logger.add(new transports.Syslog(slOptions));
+        }
         if (config.loki.enabled) {
+            const standardLabels = { "appname": appname };
+            const labels = {
+                ...standardLabels, ...config.loki.labels
+            };
             const lokiTransport = new LokiTransport({
                 host: config.loki.url,
                 level: config.loki.level,
@@ -14,9 +34,10 @@ class LoggerFactory {
                 gracefulShutdown: false,
                 format: format.combine(
                     format.splat(),
-                    format.simple()
-                ),
-                labels: { appname: appname }
+                    format.printf((info) => {
+                        return `${info.message}`;
+                    })),
+                labels: labels
             });
             this.logger.add(lokiTransport)
         };
